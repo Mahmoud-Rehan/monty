9ccb0b064406b5bb26668d1ff926531504b9c5f7
Mahmoud-Rehan <mahmoudreha38@example.com>
2024-02-04 14:17:00 +0000

    C Language

---
+++ get_instructions.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+
+void get_instructions(void)
+{
+	int n;
+	instruction_t instructions[] = {
+		{"push", &push}, {"pop", &pop},
+		{"pint	", &pint}, {"swap", &swap},
+		{"nop", &nop}, {"add", &add},
+		{"pall", &pall}, {"sub", &sub},
+		{"div", &_div}, {"mul", &mul},
+		{"rotl", &rotl}, {"rotr", &rotr},
+		{"stack", &stack}, {"queue", &queue},
+		{"pstr", &pstr}, {"pchar", &pchar},
+		{"mod", &mod},
+		{NULL, NULL}
+	};
+
+	if (arguments->tokens_num == 0)
+	{
+		return;
+	}
+
+	for (n = 0; instructions[n].opcode != NULL; n++)
+	{
+		if (strcmp(instructions[n].opcode, arguments->tokens[0]) == 0)
+		{
+			arguments->instructions->opcode = instructions[n].opcode;
+			arguments->instructions->f = instructions[n].f;
+			return;
+		}
+	}
+
+	instructions_error();
+}
---
+++ instructions_error.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+
+void instructions_error(void)
+{
+	dprintf(2, "L%d: unknown instruction %s\n", arguments->line_num, arguments->tokens[0]);
+	close_stream();
+	free_tokens();
+	free_args();
+	exit(EXIT_FAILURE);
+}
