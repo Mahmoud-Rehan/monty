7e0031a03150150675425eccb1cba3ac40200cd0
Mahmoud-Rehan <mahmoudreha38@example.com>
2024-02-04 13:57:08 +0000

    Header File

---
+++ monty.h
@@ -0,0 +1,74 @@
+#ifndef MONTY_H_
+#define MONTY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+ * struct args - Hold the arguments.
+ * @stream: The file stream.
+ * @line: The line read from stream.
+ *
+ * Description: Hold arguments used in
+ * functions of the project.
+ */
+
+typedef struct args
+{
+	FILE *stream;
+	char *line;
+	unsigned int line_num;
+	char **tokens;
+	int tokens_num;
+	instruction_t *instructions;
+} args_t;
+
+
+extern args_t *arguments;
+
+
+/* FUNCTONS PROTOTYPES */
+void initialize_args(void);
+void check_args(int argc);
+void _getstream(char *file_name);
+
+void getstream_error(char *file_name);
+void malloc_error(void);
+#endif
