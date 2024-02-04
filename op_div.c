f9ec1adda5c70a0436a3f8d7a218148873be65d2
Mahmoud-Rehan <mahmoudreha38@example.com>
2024-02-04 23:24:57 +0000

    C Language

---
+++ op_div.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second;
+
+	(void)stack;
+
+	if (arguments->stack_len < 2)
+	{
+		dprintf(2, "L%d: can't div, stack too short\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+
+	first = arguments->h;
+	second = first->next;
+
+	if (first->n == 0)
+	{
+		dprintf(2, "L%d: division by zero\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+
+	second->n = second->n / first->n;
+	delete_stack_top();
+	arguments->stack_len -= 1;
+}
---
+++ op_mul.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second;
+
+	(void)stack;
+
+	if (arguments->stack_len < 2)
+	{
+		dprintf(2, "L%d: can't sub, stack too short\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+
+	first = arguments->h;
+	second = first->next;
+	second->n = second->n * first->n;
+	delete_stack_top();
+	arguments->stack_len -= 1;
+}
