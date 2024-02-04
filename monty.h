7015f34276acedf18d63019aafbb106e338059c3
Mahmoud-Rehan <mahmoudreha38@example.com>
2024-02-04 20:15:43 +0000

    Header File

--- monty.h
+++ monty.h
@@ -61,6 +61,9 @@ typedef struct args
 	unsigned int line_num;
 	char **tokens;
 	int tokens_num;
+	stack_t *h;
+	int stack_len;
+	int stack;
 	instruction_t *instructions;
 } args_t;
 
@@ -83,33 +86,33 @@ void malloc_error(void);
 /* OPCODE FUNCTIONS */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void pint(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void pop(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void swap(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void add(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void nop(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void sub(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void _div(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void mul(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void mod(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void pchar(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void pstr(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void rotl(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void rotr(stack_t **stack, unsigned int line_number);
-
+/*---------------------------------------------------*/
 void stack(stack_t **stack, unsigned int line_number);
 void queue(stack_t **stack, unsigned int line_number);
 
