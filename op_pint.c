25f44a487abf1d9ed478dc76f8b0a5397ad64a0b
Mahmoud-Rehan <mahmoudreha38@example.com>
2024-02-05 01:18:50 +0000

    C Language

--- op_pint.c
+++ op_pint.c
@@ -14,7 +14,7 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (arguments->h == NULL)
 	{
-		dprintf(2, "L%d: can't pint, stack empty", line_number);
+		dprintf(2, "L%d: can't pint, stack empty\n", line_number);
 		free_all();
 		exit(EXIT_FAILURE);
 	}
